1c5964dd2651095f8c785f11ec5845eba740a59b
MygoathubID <taeseong0929@naver.com>
2024-06-28 20:52:46 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,15 +1,36 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # # # # #
+        # . . . #
+        . # # # .
+        `)
     music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
-        . . # # .
-        # . # . #
-        . . # . #
-        # . # . #
-        . . # # .
+        # . . . #
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
         `)
 })
 input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . # . .
+        `)
     music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
 })
+while (input.isGesture(Gesture.ScreenUp)) {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.yawn), music.PlaybackMode.UntilDone)
+    basic.showString("zzzzz")
+}
+while (input.isGesture(Gesture.ScreenDown)) {
+    music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+}
